34844fb74f2107e1e5ce76b2af783ca616ca82e0
Praneeth <praneeth.nellutla16@gmail.com>
2023-10-06 20:35:35 +0530

    changes in linter for best practice

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flora_cure/components/post.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
